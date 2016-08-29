5ce6860d437a83a208ecf0b2cb87c7fc154303c9
Daniel Blazevski <daniel.blazevski@gmail.com>
2016-08-29 19:38:04 -0400

    commented out debug print lines

--- kinesis-read-dynamodb.py
+++ kinesis-read-dynamodb.py
@@ -7,7 +7,7 @@ import decimal
 
 ## Connent to the kinesis stream
 kinesis = boto3.client("kinesis")
-shard_id = 'shardId-000000000000' #we only have one shard!
+shard_id = 'shardId-000000000000' #only one shard
 shard_it = kinesis.get_shard_iterator(StreamName="twitter", ShardId=shard_id, ShardIteratorType="LATEST")["ShardIterator"]
 
 dynamodb = boto3.resource('dynamodb')
@@ -17,7 +17,6 @@ while 1==1:
 	out = kinesis.get_records(ShardIterator=shard_it, Limit=100)
 	for record in out['Records']:
 		if 'entities' in json.loads(record['Data']):
-			print(json.loads(record['Data'])['entities']['hashtags'])
 			htags = json.loads(record['Data'])['entities']['hashtags']
 			if htags:
  				for ht in htags:
@@ -46,7 +45,6 @@ while 1==1:
                                         		ReturnValues="UPDATED_NEW"
                                 		)    
 	shard_it = out["NextShardIterator"]
-#	print out;
 	time.sleep(1.0)
 
 

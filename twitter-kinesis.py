cd33320e6e56ab2f3e93c061e0175f2d10649f6d
danielblazevski <daniel.blazevski@gmail.com>
2016-10-05 14:19:40 -0400

    cleaned up twitter-kinesis.py

--- twitter-kinesis.py
+++ twitter-kinesis.py
@@ -15,17 +15,9 @@ access_token_secret = twitterCreds.access_token_secret
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 kinesis = boto3.client('kinesis')
-#kinesis = kinesis.connect_to_region("us-east-1")
-
-# if stream is not created, uncomment this line
-# kinesis.create_stream(name = "twitter", number_shards = 1)
 
 r = api.request('statuses/filter', {'locations':'-90,-90,90,90'})
-tweets = []
-for item in r:
-	record = {'Data':json.dumps(item), 'PartitionKey':"filler"}
-	tweets.append(record)
-	kinesis.put_record(StreamName="twitter", Data=json.dumps(item), PartitionKey="filler")
 
-#kinesis.put_records(Records=tweets,StreamName="twitter")
+for item in r:
+	kinesis.put_record(StreamName="twitter", Data=json.dumps(item), PartitionKey="filler")
 
